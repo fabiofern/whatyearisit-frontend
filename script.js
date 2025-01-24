6ab5f6b23cd894bdf3454ee96265411b670330f4
fabio <fabio.f@outlook.fr>
2025-01-24 16:34:44 +0100

    test

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch("https://whatyearisit-backend-nine-iota.vercel.app/year")
+fetch("https://whatyearisit-backend-three-psi.vercel.app/year")
     .then((response) => response.json())
     .then((data) => {
         document.querySelector("#year").textContent = data.year;
