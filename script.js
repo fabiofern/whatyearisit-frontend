943b8f34d221a0f766197b3a57929ef69d87d843
fabio <fabio.f@outlook.fr>
2025-01-24 15:18:44 +0100

    commit updated2

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const year = document.querySelector("#year");
 
-fetch("https://whatyearisit-backend-nwqn.vercel.app/")
+fetch("https://whatyearisit-backend-nine-iota.vercel.app")
     .then((response) => response.json())
     .then((data) => {
         year.textContent(data.year);
     });
 // test 2
 // test
+cd ..cd
